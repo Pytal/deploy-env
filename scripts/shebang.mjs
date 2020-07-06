b3c1f1eaa55dcd84c69c6ae4546ab9695255a8b3
Chris <chrng2@gmail.com>
2020-07-06 12:03:53 -0700

    Add scripts/shebang

---
+++ scripts/shebang.mjs
@@ -0,0 +1,7 @@
+import { readFile, writeFile } from 'fs/promises'
+
+const filename = process.argv[2]
+const shebang = '#!/usr/bin/env node\n'
+
+const contents = await readFile( filename, 'utf-8' )
+await writeFile( filename, shebang + contents )
