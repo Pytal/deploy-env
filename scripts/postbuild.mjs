abcecc11dc67e40051231dd6a208d395a5fce82b
Chris <chrng2@gmail.com>
2020-07-06 18:10:01 -0700

    remove scripts/postbuild in favour of shebang

--- scripts/postbuild.mjs
+++
@@ -1,7 +0,0 @@
-import { readFile, writeFile } from 'fs/promises'
-
-const filename = process.argv[2]
-const shebang = '#!/usr/bin/env node'
-
-const contents = await readFile( filename, 'utf-8' )
-await writeFile( filename, shebang + '\n' + contents )
