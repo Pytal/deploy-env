846f5d3f19546b11812191929083ec53d96060ec
Christopher Ng <chrng8@gmail.com>
2021-08-15 14:34:14 -0700

    Configure test coverage

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,14 @@ module.exports = {
   transform: {
     '^.+\\.ts$': 'esbuild-jest',
   },
+  collectCoverage: true,
+  coverageProvider: 'babel',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 }
